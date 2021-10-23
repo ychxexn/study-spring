f0709c968036eac6b6bde096fbdfc8210ccc9dab
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-23 17:08:39 +0900

    애플리케이션 컨텍스트 추가

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -1,11 +1,17 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+	@Bean
 	public UserDAO userDAO() {
 		return new UserDAO(connectionMaker());
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
--- src/springbook/user/dao/UserDAOTest.java
+++ src/springbook/user/dao/UserDAOTest.java
@@ -2,10 +2,16 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class UserDAOTest {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		UserDAO dao = new DaoFactory().userDAO();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		// @Bean 이 붙은 메소드 이름이 빈의 이름이 된다.
+		// getBean은 Object 타입으로 리턴하게 돼있어서 제네릭 메소드 방식을 사용해 리턴 타입을 주어 지저분한 캐스팅 코드를 사용하지 않았다.
+		UserDAO da = context.getBean("userDAO", UserDAO.class);
 	}
 
 }
