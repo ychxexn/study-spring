0b8a14c009f280382cc41a80fbd9ce0d0fd47e5e
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-24 22:06:00 +0900

    스프링 테스트 추가

--- src/springbook/user/dao/UserDAOTest.java
+++ src/springbook/user/dao/UserDAOTest.java
@@ -9,10 +9,14 @@ import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
@@ -28,9 +32,15 @@ import springbook.user.domain.User;
  * 7. 모든 테스트의 결과를 종합해서 돌려준다. 
  * 
  */
+@RunWith(SpringJUnit4ClassRunner.class)						// 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+@ContextConfiguration(locations="/applicationContext.xml")	// 테스트 컨텍스트가 자동으로 만들어 줄 애플리케이션 컨텍스트의 위치 지정
 public class UserDAOTest {
+//	@Autowired
+//	private ApplicationContext context;
+	
 	// 픽스처 : 테스트를 수행하는 데 필요한 정보나 오브젝트
 	// 반복적으로 사용되기 때문에 @Before 메소드를 이용해 생성해둔다.
+	@Autowired
 	private UserDAO dao;
 	private User user1;
 	private User user2;
@@ -38,9 +48,10 @@ public class UserDAOTest {
 	
 	@Before
 	public void setUp() {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		//System.out.println(this.context);	// 항상 같음
+		//System.out.println(this);			// 매번 다름
 		
-		this.dao = context.getBean("userDAO", UserDAO.class);
+		//this.dao = context.getBean("userDAO", UserDAO.class);
 		this.user1 = new User("chaeeun1", "윤채은1", "dbscodms1");
 		this.user2 = new User("chaeeun2", "윤채은2", "dbscodms2");
 		this.user3 = new User("chaeeun3", "윤채은3", "dbscodms3");
