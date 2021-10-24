6d78e398804178bb2e0d9f068ef7210791688b18
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-24 16:36:41 +0900

    XML을 이용한 애플리케이션 컨텍스트 추가

--- src/springbook/user/dao/UserDAOTest.java
+++ src/springbook/user/dao/UserDAOTest.java
@@ -4,11 +4,15 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDAOTest {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		// XML 설정파일을 사용하도록 수정
+		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
 		// @Bean 이 붙은 메소드 이름이 빈의 이름이 된다.
 		// getBean은 Object 타입으로 리턴하게 돼있어서 제네릭 메소드 방식을 사용해 리턴 타입을 주어 지저분한 캐스팅 코드를 사용하지 않았다.
 		UserDAO da = context.getBean("userDAO", UserDAO.class);
