221bd4d8c34d269c9a8e83846e563677e28a49c3
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-27 22:09:57 +0900

    DataAccessException 활용 - DB에 독립적인 예외처리

--- src/springbook/user/dao/UserDAO.java
+++ src/springbook/user/dao/UserDAO.java
@@ -1,72 +1,12 @@
 package springbook.user.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-
-import com.mysql.jdbc.MysqlErrorNumbers;
-
 import springbook.user.domain.User;
-import springbook.user.exception.DuplicateUserIdException;
 
-public class UserDAO {
-
-	private RowMapper<User> userMapper = 
-		new RowMapper<User>() {
-			@Override
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				
-				return user;
-			}
-		};
-		
-	private JdbcTemplate jdbcTemplate;
-	
-	public UserDAO() {
-	}
-	
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-
-
-	public void add(User user) throws DuplicateUserIdException {
-		try {
-			this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)"
-					, user.getId(), user.getName(), user.getPassword());
-			// 컴파일 에러를 피하기 위해 넣음
-			throw new SQLException();
-		}catch(SQLException e) {
-			if(e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
-				throw new DuplicateUserIdException(e);	// 예외 전환
-			}else {
-				throw new RuntimeException(e);			// 예외 포장
-			}
-		}
-	}
-	
-	public User get(String id) throws SQLException {
-		return this.jdbcTemplate.queryForObject("select * from users where id=?", new Object[] {id}, this.userMapper);
-	}
-	
-	public void deleteAll() throws SQLException{
-		this.jdbcTemplate.update("delete from users");
-	}
-	
-	public int getCount() throws SQLException{
-		return this.jdbcTemplate.queryForInt("select count(*) from users");
-	}
-
-	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
-	}
+public interface UserDAO {
+	public void add(User user);
+	public User get(String id);
+	public List<User> getAll();
+	public void deleteAll();
+	public int getCount();
 }
--- src/springbook/user/dao/UserDAOTest.java
+++ src/springbook/user/dao/UserDAOTest.java
@@ -6,11 +6,17 @@ import static org.junit.Assert.assertThat;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -38,6 +44,10 @@ public class UserDAOTest {
 	// 반복적으로 사용되기 때문에 @Before 메소드를 이용해 생성해둔다.
 	@Autowired
 	private UserDAO dao;
+	
+	@Autowired
+	DataSource dataSource;
+	
 	private User user1;
 	private User user2;
 	private User user3;
@@ -125,6 +135,29 @@ public class UserDAOTest {
 		assertThat(user1.getId(), is(user2.getId()));
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test(expected=DuplicateKeyException.class)
+	public void duplicateKey() {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		dao.add(user1);
+	}
+	
+	@Test
+	public void sqlExceptionTranslate() {
+		dao.deleteAll();
+		
+		try {
+			dao.add(user1);
+			dao.add(user1);
+		}catch(DuplicateKeyException ex) {
+			SQLException sqlEx = (SQLException)ex.getRootCause();
+			SQLExceptionTranslator set = new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+			
+			assertThat(set.translate(null, null, sqlEx), is(DuplicateKeyException.class));
+		}
 	}
 	
 
