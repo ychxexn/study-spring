2592687be2b9ff525c960142303bfa03aa4c44fa
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-28 23:11:15 +0900

    level 필드 추가

--- src/springbook/user/domain/User.java
+++ src/springbook/user/domain/User.java
@@ -1,10 +1,16 @@
 package springbook.user.domain;
 
 public class User {
-
+	
 	String id;
 	String name;
 	String password;
+	
+	private static final int BASIC = 1;
+	private static final int SILVER = 2;
+	private static final int GOLD = 3;
+	
+	int level;
 	
 	// 자바빈의 규약을 따르는 클래스에 생성자를 명시적으로 추가할 때에는 디폴트 생성자를 함께 정의해야한다.
 	public User() {
@@ -34,6 +40,9 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public void setLevel(int level) {
+		this.level = level;
 	}
 	
 }
