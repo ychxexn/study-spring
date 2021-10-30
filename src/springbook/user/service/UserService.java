a74ab4ad5d89c42f8b706b7363edcff21db1c95f
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-30 20:01:24 +0900

    User 리팩토링

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -18,24 +18,30 @@ public class UserService {
 		List<User> users = userDAO.getAll();
 		
 		for(User user : users) {
-			Boolean changed = null;
-			
-			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-				user.setLevel(Level.SILVER);
-				changed = true;
-			}else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-				user.setLevel(Level.GOLD);
-				changed = true;
-			}else if(user.getLevel() == Level.GOLD) {
-				changed = false;
-			}else {
-				changed = false;
-			}
-			
-			if(changed) {
-				userDAO.update(user);
+			if(canUpgradeLevel(user)) {
+				upgradeLevel(user);
 			}
 		}
+	}
+
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		
+		switch(currentLevel) {
+			case BASIC:
+				return (user.getLogin() >= 50);
+			case SILVER:
+				return (user.getRecommend() >= 30);
+			case GOLD:
+				return false;
+			default:
+				throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+		}
+	}
+	
+	private void upgradeLevel(User user) {
+		user.upgradeLevel();
+		userDAO.update(user);
 	}
 
 	public void add(User user) {
