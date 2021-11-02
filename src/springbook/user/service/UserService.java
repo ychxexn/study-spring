ab9fd3cbbcc31c81c2113b53b5100a026990691c
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-11-02 21:59:44 +0900

    스프링 트랜잭션 추상화 API 적용

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import springbook.user.dao.UserDAO;
@@ -30,9 +35,9 @@ public class UserService {
 	}
 	
 	public void upgradeLevels() throws Exception {
-		TransactionSynchronizationManager.initSynchronization();
-		Connection c = DataSourceUtils.getConnection(dataSource);
-		c.setAutoCommit(false);
+		PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+		
+		TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 		
 		try {
 			List<User> users = userDAO.getAll();
@@ -42,17 +47,11 @@ public class UserService {
 					upgradeLevel(user);
 				}
 			}
-			
-			c.commit();
+			transactionManager.commit(status);
 		}catch(Exception e) {
-			c.rollback();
+			transactionManager.rollback(status);
 			throw e;
-		}finally {
-			DataSourceUtils.releaseConnection(c, dataSource);
-			TransactionSynchronizationManager.unbindResource(this.dataSource);
-			TransactionSynchronizationManager.clearSynchronization();
 		}
-		
 	}
 
 	private boolean canUpgradeLevel(User user) {
