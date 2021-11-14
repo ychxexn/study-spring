721fea8eaa103794068b745349d85ea2b7115124
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-11-14 13:57:16 +0900

    스프링 ProxyFactoryBean 실습

---
+++ src/springbook/learningtest/jdk/DynamicProxyTest.java
@@ -0,0 +1,70 @@
+package springbook.learningtest.jdk;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import springbook.proxy.jdk.Hello;
+import springbook.proxy.jdk.HelloTarget;
+import springbook.proxy.jdk.UppercaseHandler;
+
+public class DynamicProxyTest {
+	@Test
+	public void simpleProxy() {
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader(),
+				new Class[] {Hello.class},
+				new UppercaseHandler(new HelloTarget()));
+	}
+	
+	@Test
+	public void proxyFactoryBean() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		pfBean.addAdvice(new UppercaseAdvice());
+		
+		Hello proxiedHello = (Hello)pfBean.getObject();
+		
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	static class UppercaseAdvice implements MethodInterceptor {
+		public Object invoke(MethodInvocation invocation) throws Throwable{
+			String ret = (String)invocation.proceed();
+			return ret.toUpperCase();
+		}
+	}
+	
+	static interface Hello {
+		String sayHello(String name);
+		String sayHi(String name);
+		String sayThankYou(String name);
+	}
+	
+	static class HelloTarget implements Hello {
+		@Override
+		public String sayHello(String name) {
+			return "Hello " + name;
+		}
+
+		@Override
+		public String sayHi(String name) {
+			return "Hi " + name;
+		}
+
+		@Override
+		public String sayThankYou(String name) {
+			return "Thank You " + name;
+		}
+		
+	}
+}
